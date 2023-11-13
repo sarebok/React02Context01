db8f3dcca30ab5b04ba65b65390c18025976bfc8
sarebok <bortonae@hotmail.com>
2023-11-13 14:26:47 -0300

    first commit

---
+++ src/context/AppContext.jsx
@@ -0,0 +1,14 @@
+import { createContext, useState } from "react";
+
+export const AppContext = createContext();
+
+const ContextProvider = ({ children }) => {
+  const [images, setImages] = useState();
+  const [liked, setLiked] = useState(false);
+
+  const state = { images, setImages, liked, setLiked };
+
+  return <AppContext.Provider value={state}>{children}</AppContext.Provider>;
+};
+
+export default ContextProvider;
