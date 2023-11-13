a6afcb68145d2f95a891953cbd43dfeb4f5672a7
sarebok <bortonae@hotmail.com>
2023-11-13 16:01:59 -0300

    call photos.json in home

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,6 +1,22 @@
 import Gallery from "../components/Gallery";
+import { useEffect } from "react";
 
 const Home = () => {
+  async function getPhotos() {
+    try {
+      const response = await fetch("/photos.json");
+      const data = await response.json();
+      const photosData = data.photos;
+      console.log(photosData);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getPhotos();
+  }, []);
+
   return (
     <div className="App">
       <h1>Natural Pic</h1>
